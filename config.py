9bd9b0ae682bd4a49d4b5a8667e278bdc39d5aa6
fungusAcademy <grivus12@yandex.ru>
2025-09-15 22:30:35 +1000

    Changed code to first get bot token from environment variables

--- config.py
+++ config.py
@@ -8,16 +8,24 @@ def get_database_url():
         if db_url.startswith('postgresql://'):
             db_url = db_url.replace('postgresql://', 'postgres://', 1)
         return db_url
+     
+    load_dotenv()
     if "DATABASE_DSN" in os.environ:
         return os.environ['DATABASE_DSN']
     
     raise ValueError("Cannot find DATABASE URL or DSN in env var!!!")
 
-load_dotenv()
+def get_bot_token():
+    if 'BOT_TOKEN' in os.environ:
+        return os.environ['BOT_TOKEN']
+    
+    load_dotenv()
+    if 'BOT_TOKEN' in os.environ:
+        return os.environ['BOT_TOKEN']
+    
+    raise ValueError("Cannot find BOT_TOKEN!")
 
-BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+BOT_TOKEN = get_bot_token()
 DATABASE_DSN = get_database_url()
 # DATABASE_DSN = os.getenv("DATABASE_DSN", "postgresql://user:pass@localhost/dbname")
-
-if not BOT_TOKEN:
-    raise ValueError("Cannot find BOT_TOKEN in .env file")
